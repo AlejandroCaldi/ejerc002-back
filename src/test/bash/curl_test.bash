7865e0301896fba5a6ae20f9c784a5e4770cb532
curso07 <alejandro.cald@gmail.com>
2025-02-17 10:47:13 +0100

    Test de bash actualizado.

--- src/test/bash/curl_test.bash
+++ src/test/bash/curl_test.bash
@@ -2,31 +2,53 @@
 
 BASE_URL="http://localhost:8080/api/boligrafo"
 
-# Create a new boligrafo
-echo "Creating a new boligrafo..."
+echo "creando un nuevo boligrafo..."
 CREATE_RESPONSE=$(curl -s -X POST "$BASE_URL" \
   -H "Content-Type: application/json" \
-  -d '{"color":"azul", "escribe": true, "marca":"Bic"}')
+  -d '{"color":"azul", "escribe": true, " nombre":"Mi primera Bic"}')
+
+echo ""
+
+CREATE_RESPONSE=$(curl -s -X POST "$BASE_URL" \
+  -H "Content-Type: application/json" \
+  -d '{"color":"verde", "escribe": true, "nombre":"Mi nueva Watermark"}')
+
+echo ""
+echo ""
 
 echo "Create Response: $CREATE_RESPONSE"
 BOLIGRAFO_ID=$(echo "$CREATE_RESPONSE" | jq -r '.id')
 
-# Read a boligrafo by ID
-echo "Reading boligrafo with ID: $BOLIGRAFO_ID"
+echo ""
+echo ""
+
+echo "Leyendo boligrafo con ID: $BOLIGRAFO_ID"
+echo ""
 curl -s -X GET "$BASE_URL/$BOLIGRAFO_ID" -H "Content-Type: application/json"
 
-# List all boligrafos
-echo "Listing all boligrafos..."
+echo ""
+echo ""
+
+echo "Listado de todos los boligrafos..."
+echo ""
 curl -s -X GET "$BASE_URL" -H "Content-Type: application/json"
 
-# Update the boligrafo
-echo "Updating boligrafo..."
+echo ""
+echo ""
+
+echo "Update de boligrafo..."
 curl -s -X PUT "$BASE_URL" \
   -H "Content-Type: application/json" \
   -d '{"id":1, "color":"rojo", "marca":"Bic"}'
 
-# Delete the boligrafo
-echo " Deleting boligrafo with ID: $BOLIGRAFO_ID"
+echo ""
+echo ""
+
+echo "Borrado de un boligrafo con ID: $BOLIGRAFO_ID"
+
+echo ""
+
+
 curl -s -X DELETE "$BASE_URL/$BOLIGRAFO_ID" -H "Content-Type: application/json"
 
-echo "Test completed!"
+echo "Test terminado."
